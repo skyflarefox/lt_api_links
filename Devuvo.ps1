9b41df505d891165a77d8f18e1f9e9c70a4773e3
Tesla697 <96721065+Tesla697@users.noreply.github.com>
2026-04-29 17:42:22 +0530

    Update Devuvo.ps1

--- Devuvo.ps1
+++ Devuvo.ps1
@@ -23,7 +23,7 @@ $customLaunchers = @{
     # Resident Evil Requiem (Capcom)
     "3764200" = @{ Exe = "START_WITH_THIS_EXE.exe"; GameName = "Resident Evil Requiem" }
     # Monster Hunter Stories 3: Twisted Reflection (Capcom)
-    "2852190" = @{ Exe = "START_MHS3.exe"; GameName = "Monster Hunter Stories 3: Twisted Reflection" }
+    "2852190" = @{ Exe = "tokeer_launcher.exe"; GameName = "Monster Hunter Stories 3: Twisted Reflection" }
     # Maneater (Denuvo + tokeer)
     "629820"  = @{ Exe = "tokeer_launcher.exe"; GameName = "Maneater" }
     # FAR: Changing Tides (Denuvo + tokeer)
